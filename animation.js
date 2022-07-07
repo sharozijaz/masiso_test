3afb40bdbebd95c4632f0bf61a94f4d7d5d78885
Sharoz Ijaz <sherry@sharoz.dev>
2022-07-07 15:49:21 +0500

    animation

---
+++ animation.js
@@ -0,0 +1,16 @@
+var tl = gsap.timeline();
+
+tl.from(".animate__hero", { duration: 2, x: 900, ease: "power4.out" });
+
+gsap.from(".left", {
+  duration: 1,
+  x: -300,
+  ease: "power1.out",
+  stagger: 0.25,
+});
+gsap.from(".right", {
+  duration: 1,
+  x: 300,
+  ease: "power1.out",
+  stagger: 0.25,
+});
